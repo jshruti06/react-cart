dcb9741014723e245c6f94291f0c9697b7bcdfb2
Shruti Jain <shrutijain062001@gmail.com>
2022-07-06 15:28:58 +0530

    cart_commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+// import About from './pages/About'
+import ProductsPage from './pages/ProductsPage'
+import Navigation from './components/Navigation'
+import Cart from './pages/Cart'
+import SingleProduct from './pages/SingleProduct'
+import {CartContext} from './CartContext'
+import { useEffect,useState } from 'react'
+import { getCart,storeCart } from './helpers'
+
+
+const App=()=>{
+
+    const [cart,setCart]=useState({});
+    //fetch cart from local storage
+    
+    useEffect(()=>{
+       getCart().then(cart=>{
+        setCart(JSON.parse(cart))
+       });
+       
+       
+    },[]);
+
+
+    useEffect(()=>{
+        storeCart(JSON.stringify(cart));
+     },[cart]);
+
+    return (
+    <>
+        <Router>
+            <CartContext.Provider value={{ cart,setCart}}>
+                <Navigation/>
+                <Routes>
+                    <Route path="/" element = {<Home/>} ></Route>
+                    {/* <Route path="/about" element = {<About/>} ></Route> */}
+                    <Route path="/products" element = {<ProductsPage/>} ></Route>
+                    <Route path="/products/:_id" element={<SingleProduct/>}></Route>
+                    <Route path="/cart" element = {<Cart/>} ></Route>
+                </Routes>
+            </CartContext.Provider>
+        </Router>
+    </>
+    )
+}
+
+export default App;
